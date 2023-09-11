2e0bc8ec39765bfb5c8b0540d016b060dd5850fb
Mikko R <yashoo_11@hotmail.fi>
2023-09-11 18:57:43 +0300

    add local files

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Set Pug as the view engine
+app.set('view engine', 'pug');
+
+// Define the directory where your Pug templates are located
+app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files from the "public" directory
+app.use(express.static(__dirname + '/public'));
+
+// Define a route to render the Pug template
+app.get('/', (req, res) => {
+    // Render the "index.pug" template
+    res.render('index');
+});
+
+app.listen(port, () => {
+    console.log(`Server is http://localhost:${port}`);
+});
