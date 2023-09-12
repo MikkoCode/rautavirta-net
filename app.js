4e7ff16d44f920d58f12a2ebddd4a4269edf5851
Mikko R <yashoo_11@hotmail.fi>
2023-09-12 12:26:55 +0300

    language / locale support

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const path = require('path');
+const i18n = require('i18n');
+const cookieParser = require('cookie-parser');
+require("domain");
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Configure i18n middleware
+i18n.configure({
+    locales: ['fi', 'en'], // Supported languages (Finnish and English)
+    directory: __dirname + '/locales', // Path to language files
+    cookie: 'language', // Cookie name for storing language preference
+});
+
+// Initialize cookie-parser middleware
+app.use(cookieParser());
+
+// Initialize i18n middleware
+app.use(i18n.init);
 
 // Set Pug as the view engine
 app.set('view engine', 'pug');
@@ -13,13 +29,43 @@ app.set('views', path.join(__dirname, 'views'));
 // Serve static files from the "public" directory
 app.use(express.static(__dirname + '/public'));
 
-// Define a route to render the Pug template
-app.get('/', (req, res) => {
-    // Render the "index.pug" template
-    res.render('index');
+// Define a route to change the language
+// Define a route to change the language
+app.get('/switch-language/:lang', (req, res) => {
+    const { lang } = req.params;
+    res.cookie('language', lang);
+    i18n.setLocale(lang); // Explicitly set the locale
+    console.log(`Language change route accessed. Current locale: ${i18n.getLocale()}`);
+    res.redirect('/');
 });
 
-app.get('*', function(req, res){
+// Define a route to render the Pug template
+app.get('/', (req, res) => {
+    if (!req.cookies.language) {
+        const reqlocale = i18n.getLocale(); // Set the locale from the Accept-Language header
+        console.log('No cookie found, request locale: ' + reqlocale);
+        res.cookie('language', reqlocale);
+        i18n.setLocale(reqlocale);
+        res.setLocale(i18n.getLocale()); // Set the locale from the cookie
+        console.log(`Rendering Pug template. Current locale: ${res.getLocale()}`);
+        res.render('index', {
+            welcomeMessage: res.__('welcome_message'),
+            about: res.__('about'),
+            contact: res.__('contact'),
+            languageMessage: res.__('language_message')
+        });
+    } else {
+        res.setLocale(i18n.getLocale()); // Set the locale from the cookie
+        console.log(`Rendering Pug template. Current locale: ${res.getLocale()}`);
+        res.render('index', {
+            welcomeMessage: res.__('welcome_message'),
+            about: res.__('about'),
+            contact: res.__('contact') });
+    }
+
+});
+
+app.get('*', function (req, res) {
     res.render('notfound');
 });
 
