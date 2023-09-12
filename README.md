9816f8dc78e1fab62292ffa5d5a04732fd474d52
Mikko R <yashoo_11@hotmail.fi>
2023-09-12 21:32:06 +0300

    lotta changes

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# rautavirta-net
- My portfolio website project
+# Mikko Rautavirta's Portfolio Website
+
+## Introduction
+
+This repository contains the source code for Mikko Rautavirta's personal portfolio website. The website is built using Node.js (Express) and the Pug template engine. It showcases Mikko's skills, projects, and contact information, offering both English and Finnish translations.
+
+## Table of Contents
+
+1. [Installation](#installation)
+2. [Usage](#usage)
+3. [Structure](#structure)
+4. [Contributing](#contributing)
+5. [License](#license)
+6. [Contact](#contact)
+
+## Installation
+
+### Prerequisites
+- Node.js (20.6.0)
+- npm (7.20.3)
+
+### Steps
+1. Clone the repository: `git clone https://github.com/your-repository-url.git`
+2. Navigate to the project directory: `cd your-repository-name`
+3. Install dependencies: `npm install`
+4. Run the server: `npm start`
+
+## Usage
+
+Visit `http://localhost:3000` to view the website in your browser.
+
+## Structure
+
+- `app.js`: The main server file.
+- `public/`: Directory containing static files such as CSS, JavaScript, and images.
+- `views/`: Directory containing Pug templates for the different sections of the website.
+- `locales/`: Directory containing JSON files for English (`en.json`) and Finnish (`fi.json`) translations.
+- `assets/`: Directory containing assets like profile pictures and favicons.
+
+### Directories and Files
+
+#### `views/`
+- `layout.pug`: Basic structure of the web pages.
+- `header.pug`: Template for the navbar.
+- `footer.pug`: Template for the footer.
+- `about.pug`: Template for the "About Me" section.
+- `skills.pug`: Template for the "Skills" section.
+- `projects.pug`: Template for the "Projects" section.
+- `contact.pug`: Template for the "Contact" section.
+- `notfound.pug`: Template for the 404 - Not Found page.
+
+#### `public/stylesheets/`
+- `nav.css`: Styles for the navbar.
+- `styles.css`: General styles across the website.
+- (Additional CSS files)
+
+#### `public/javascripts/`
+- `script.js`: Client-side JavaScript file for handling various functionalities.
+
+#### `public/images/`
+- `profilepic.jpg`: Profile picture used in the "About" section.
+- `favicon.ico`: Website favicon.
+
+#### `locales/`
+- `en.json`: English translation file.
+- `fi.json`: Finnish translation file.
+
+
--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const i18n = require('i18n');
 const cookieParser = require('cookie-parser');
-require("domain");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,129 +9,52 @@ const port = process.env.PORT || 3000;
 // Configure i18n middleware
 i18n.configure({
     locales: ['fi', 'en'], // Supported languages (Finnish and English)
-    directory: __dirname + '/locales', // Path to language files
+    directory: path.join(__dirname, 'locales'), // Path to language files
     cookie: 'language', // Cookie name for storing language preference
 });
 
-// Initialize cookie-parser middleware
+// Initialize middleware
 app.use(cookieParser());
-
-// Initialize i18n middleware
 app.use(i18n.init);
 
 // Set Pug as the view engine
 app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views')); // Define the directory for Pug templates
+app.use(express.static(path.join(__dirname, 'public'))); // Serve static files from the "public" directory
 
-// Define the directory where your Pug templates are located
-app.set('views', path.join(__dirname, 'views'));
-
-// Serve static files from the "public" directory
-app.use(express.static(__dirname + '/public'));
-
-// Define a route to change the language
-// Define a route to change the language
+// Language change route
 app.get('/switch-language/:lang', (req, res) => {
     const { lang } = req.params;
     res.cookie('language', lang);
-    i18n.setLocale(lang); // Explicitly set the locale
-    console.log(`Language change route accessed. Current locale: ${i18n.getLocale()}`);
+    i18n.setLocale(lang); // Set the locale
+    console.log(`Language changed to ${i18n.getLocale()}`);
     res.redirect('/');
 });
 
-// Define a route to render the Pug template
+// Render the Pug template
 app.get('/', (req, res) => {
-    if (!req.cookies.language) {
-        const reqlocale = i18n.getLocale(); // Set the locale from the Accept-Language header
-        console.log('No cookie found, request locale: ' + reqlocale);
-        res.cookie('language', reqlocale);
-        i18n.setLocale(reqlocale);
-        res.setLocale(i18n.getLocale()); // Set the locale from the cookie
-        console.log(`Rendering Pug template. Current locale: ${res.getLocale()}`);
-        res.render('pages/index', {
-            welcomeMessage: res.__('welcome_message'),
-            about: res.__('about'),
-            skills: res.__('skills'),
-            projects: res.__('projects'),
-            contact: res.__('contact'),
-            shortDescription: res.__('short_description'),
-            sourceCode: res.__('sourcecode'),
-            viewResume: res.__('view_resume'),
-            skillsItem1: res.__('skills_item_1'),
-            skillsItem1Desc: res.__('skills_item_1_desc'),
-            skillsItem2: res.__('skills_item_2'),
-            skillsItem2Desc: res.__('skills_item_2_desc'),
-            skillsItem3: res.__('skills_item_3'),
-            skillsItem3Desc: res.__('skills_item_3_desc'),
-            skillsItem4: res.__('skills_item_4'),
-            skillsItem4Desc: res.__('skills_item_4_desc'),
-            skillsItem5: res.__('skills_item_5'),
-            skillsItem5Desc: res.__('skills_item_5_desc'),
-            skillsItem6: res.__('skills_item_6'),
-            skillsItem6Desc: res.__('skills_item_6_desc'),
-            skillsItem7: res.__('skills_item_7'),
-            skillsItem7Desc: res.__('skills_item_7_desc'),
-            skillsItem8: res.__('skills_item_8'),
-            skillsItem8Desc: res.__('skills_item_8_desc'),
-            skillsItem9: res.__('skills_item_9'),
-            skillsItem9Desc: res.__('skills_item_9_desc'),
-            skillsItem10: res.__('skills_item_10'),
-            skillsItem10Desc: res.__('skills_item_10_desc'),
-            skillsItem11: res.__('skills_item_11'),
-            skillsItem11Desc: res.__('skills_item_11_desc'),
-            skillsItem12: res.__('skills_item_12'),
-            skillsItem12Desc: res.__('skills_item_12_desc'),
-            skillsItem13: res.__('skills_item_13'),
-            skillsItem13Desc: res.__('skills_item_13_desc'),
-            contactDescription: res.__('contact_description'),
-            copyright: res.__('copyright'),
-            languageMessage: res.__('language_message')
-        });
+    const reqLocale = req.cookies.language || i18n.getLocale(); // Get the preferred locale
+    res.cookie('language', reqLocale);
+    i18n.setLocale(reqLocale);
+    console.log(`Rendering Pug template. Current locale: ${reqLocale}`);
 
-    } else {
-        res.setLocale(i18n.getLocale()); // Set the locale from the cookie
-        console.log(`Rendering Pug template. Current locale: ${res.getLocale()}`);
-        res.render('pages/index', {
-            welcomeMessage: res.__('welcome_message'),
-            about: res.__('about'),
-            skills: res.__('skills'),
-            projects: res.__('projects'),
-            contact: res.__('contact'),
-            shortDescription: res.__('short_description'),
-            sourceCode: res.__('sourcecode'),
-            viewResume: res.__('view_resume'),
-            skillsItem1: res.__('skills_item_1'),
-            skillsItem1Desc: res.__('skills_item_1_desc'),
-            skillsItem2: res.__('skills_item_2'),
-            skillsItem2Desc: res.__('skills_item_2_desc'),
-            skillsItem3: res.__('skills_item_3'),
-            skillsItem3Desc: res.__('skills_item_3_desc'),
-            skillsItem4: res.__('skills_item_4'),
-            skillsItem4Desc: res.__('skills_item_4_desc'),
-            skillsItem5: res.__('skills_item_5'),
-            skillsItem5Desc: res.__('skills_item_5_desc'),
-            skillsItem6: res.__('skills_item_6'),
-            skillsItem6Desc: res.__('skills_item_6_desc'),
-            skillsItem7: res.__('skills_item_7'),
-            skillsItem7Desc: res.__('skills_item_7_desc'),
-            skillsItem8: res.__('skills_item_8'),
-            skillsItem8Desc: res.__('skills_item_8_desc'),
-            skillsItem9: res.__('skills_item_9'),
-            skillsItem9Desc: res.__('skills_item_9_desc'),
-            skillsItem10: res.__('skills_item_10'),
-            skillsItem10Desc: res.__('skills_item_10_desc'),
-            skillsItem11: res.__('skills_item_11'),
-            skillsItem11Desc: res.__('skills_item_11_desc'),
-            skillsItem12: res.__('skills_item_12'),
-            skillsItem12Desc: res.__('skills_item_12_desc'),
-            skillsItem13: res.__('skills_item_13'),
-            skillsItem13Desc: res.__('skills_item_13_desc'),
-            contactDescription: res.__('contact_description'),
-            copyright: res.__('copyright')
-        });
-    }
+    const templateData = {
+        welcomeMessage: res.__('welcome_message'),
+        about: res.__('about'),
+        skills: res.__('skills'),
+        projects: res.__('projects'),
+        contact: res.__('contact'),
+        shortDescription: res.__('short_description'),
+        sourceCode: res.__('sourcecode'),
+        viewResume: res.__('view_resume'),
+    };
 
+    // Add more properties to templateData for other localized content
+
+    res.render('pages/index', templateData);
 });
 
+// Not found route
 app.get('*', function (req, res) {
     res.render('pages/notfound');
 });
